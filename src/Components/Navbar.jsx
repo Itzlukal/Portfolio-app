6a5ad20667faa74d69d3ea660d3c6b863ca39b30
LukaL <Lukalursmanashvili398@gmail.com>
2023-11-02 14:59:35 +0400

    Created and styled Navbar a bit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className = "navbar" >
+      <ul className='ul-style'>
+        <li className='nav-buttons'>Home</li>
+        <li className='nav-buttons'>About</li>
+        <li className='nav-buttons'>Skills</li>
+        <li className='nav-buttons'>Contact</li>
+      </ul>
+    </div>
+  )
+
+}
+
+export default Navbar
